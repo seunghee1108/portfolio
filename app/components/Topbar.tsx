8c58436392760d4ced54bbcd02556cb2b93fea6b
bangseunghee <bangseunghee1108@gmail.com>
2024-02-22 22:33:56 +0900

    링크 클릭시 상자 스크롤

---
+++ app/components/Topbar.tsx
@@ -0,0 +1,37 @@
+"use client";
+import React from "react";
+import styles from "@/app/styles/topbar.module.scss";
+import Link from "next/link";
+
+function Topbar({ box1Ref, box2Ref, box3Ref, box4Ref }) {
+  const scrollToRef = (ref) => {
+    window.scrollTo({
+      top: ref.current.offsetTop,
+      behavior: "smooth",
+    });
+  };
+
+  return (
+    <div className={styles.topbar}>
+      <div className={styles.div2}>
+        <Link href="/">BANGSEUNGHEE</Link>
+      </div>
+      <div className={styles.div3}>
+        <a className={styles.link} onClick={() => scrollToRef(box1Ref)}>
+          ABOUT
+        </a>
+        <a className={styles.link} onClick={() => scrollToRef(box2Ref)}>
+          SKILL
+        </a>
+        <a className={styles.link} onClick={() => scrollToRef(box3Ref)}>
+          PROJECT
+        </a>
+        <a className={styles.link} onClick={() => scrollToRef(box4Ref)}>
+          CONTACT
+        </a>
+      </div>
+    </div>
+  );
+}
+
+export default Topbar;
