305d461ef2b8764b51bd3f5181a0263ddc46c049
bangseunghee <bangseunghee1108@gmail.com>
2024-03-04 00:32:26 +0900

    Project-content 작성

--- app/page.tsx
+++ app/page.tsx
@@ -6,6 +6,7 @@ import styles from "@/app/styles/main.module.scss";
 import Topbar from "./components/Topbar";
 import skillsData, { SkillData } from "./skill/page";
 
+
 function Index() {
   const [skillDescription, setSkillDescription] = useState("");
   const box1Ref = useRef(null);
@@ -76,10 +77,30 @@ function Index() {
           <div>
             <div className={styles.projectOne}>
               <img src="/tourmaps.png" alt="tourmaps" />
+              <div>
+                <h1>TOURMAPS</h1>
+                <p>개인 프로젝트</p>
+              </div>
             </div>
           </div>
-          <div></div>
-          <div></div>
+          <div>
+          <div className={styles.projectOne}>
+              <img src="/tourmaps.png" alt="tourmaps" />
+              <div>
+                <h1>TOURMAPS</h1>
+                <p>개인 프로젝트</p>
+              </div>
+            </div>
+          </div>
+          <div>
+          <div className={styles.projectOne}>
+              <img src="/tourmaps.png" alt="tourmaps" />
+              <div>
+                <h1>TOURMAPS</h1>
+                <p>개인 프로젝트</p>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
 
@@ -89,7 +110,34 @@ function Index() {
         ref={box4Ref}
         className={`${styles.div} ${styles.box4}`}
       >
-        <div className={styles.projectContent}>프로젝트 자세하게 설명</div>
+        <div className={styles.projectContent}>
+
+          {/* left */}
+          <div className={styles.contentImage}>
+            <img src="/tourmaps.png" alt="tourmaps" />
+            <div>
+              <h1>TOURMAPS</h1>
+              <p>설명</p>
+              <p>배포링크</p>
+              <p>GITHUB</p>
+            </div>
+          </div>
+
+          {/* right */}
+          <div className={styles.contentBox}>
+            <div>
+              <h1>프로젝트 설명</h1>
+              <p>설명입니다</p>
+              <br />
+              <h1>후기 및 소감</h1>
+              <p>후기입니다</p>
+              <br />
+              <h1>STACK</h1>
+              <p>기술입니다</p>
+            </div>
+          </div>
+
+        </div>
       </div>
 
       {/* CONTACT */}
