7b411c4ca16b95ed4a97b1ff42d34c7632d857dc
bangseunghee <bangseunghee1108@gmail.com>
2024-03-05 10:06:07 +0900

    메인페이지 프로젝트 부분 코드 수정

--- app/project/page.tsx
+++ app/project/page.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @next/next/no-img-element */
 import React from "react";
 import styles from "@/app/styles/main.module.scss";
 
@@ -9,23 +10,31 @@ const ProjectPage: React.FC<ProjectPageProps> = ({ handleProjectClick }) => {
   return (
     <div className={styles.project}>
       <div onClick={() => handleProjectClick("BOARD")}>
-        <img src="/tourmaps.png" alt="BOARD" />
+        <img src="/list.png" alt="BOARD" />
         <div className={styles.fontBox}>
           <h2>BOARD</h2>
           <h3>Solo project</h3>
         </div>
-        <p>board에 대한 설명입니다.</p>
+        <div className={styles.fontContent}>
+          <p>
+            Express와 MongoDB를 이용한 게시판 프로젝트로, 사용자는 회원가입,
+            로그인하여 글 및 댓글을 작성할 수 있습니다.
+          </p>
+          <br />
+          <h4>기술스택</h4>
+          <p>MongoDB, </p>
+        </div>
       </div>
       <div onClick={() => handleProjectClick("TOURMAPS")}>
-        <img src="/docker.png" alt="TOURMAPS" />
+        <img src="/tourmaps.png" alt="TOURMAPS" />
         <div className={styles.fontBox}>
           <h2>TOURMAPS</h2>
           <h3>Solo project</h3>
         </div>
-          <p>dd</p>
+        <p>dd</p>
       </div>
       <div onClick={() => handleProjectClick("ERP-PROJECT")}>
-        <img src="/css.png" alt="ERPPROJECT" />
+        <img src="/erp.png" alt="ERPPROJECT" />
         <div className={styles.fontBox}>
           <h2>ERP-PROJECT</h2>
           <h3>Team project</h3>
