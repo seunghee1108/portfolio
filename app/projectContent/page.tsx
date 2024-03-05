4b031bb703ee65fe5764df45483d8f700e0e5a2e
bangseunghee <bangseunghee1108@gmail.com>
2024-03-05 10:27:17 +0900

    프로젝트 자세한설명 리뷰, 기술부분 작성

--- app/projectContent/page.tsx
+++ app/projectContent/page.tsx
@@ -1,4 +1,3 @@
-// ProjectDetails.tsx
 import React from 'react';
 import styles from "@/app/styles/main.module.scss";
 
@@ -7,21 +6,27 @@ interface ProjectDetailsProps {
 }
 
 const ProjectDetails: React.FC<ProjectDetailsProps> = ({ project }) => {
-  const projectContent: { [key: string]: { description: string, link: string, github: string } } = {
+  const projectContent: { [key: string]: { description: string, link: string, github: string, review: string, technology: string } } = {
     'BOARD': {
-      description: '투어 맵 서비스에 대한 프로젝트 설명입니다.',
+      description: '회원가입, 로그인, 글작성이 가능한 게시판입니다.',
       link: '투어 맵 서비스의 배포 링크',
-      github: '투어 맵 서비스의 깃허브 링크'
+      github: '투어 맵 서비스의 깃허브 링크',
+      review: '게시판 프로젝트를 진행하면서 많은 것을 배웠습니다.',
+      technology: 'Node.js, Express, MongoDB, React'
     },
     'TOURMAPS': {
-      description: '두 번째 프로젝트에 대한 설명입니다.',
+      description: '지역별, 원하는 코스별로 여행지의 코스를 추천 받는 웹페이지입니다.',
       link: '두 번째 프로젝트의 배포 링크',
-      github: '두 번째 프로젝트의 깃허브 링크'
+      github: '두 번째 프로젝트의 깃허브 링크',
+      review: '여행지 추천 서비스를 개발하면서 사용자 경험을 고려하는 방법을 익혔습니다.',
+      technology: 'React, Next.js, MongoDB'
     },
     'ERP-PROJECT': {
-      description: '세 번째 프로젝트에 대한 설명입니다.',
+      description: '소비자의 편의와 경험을 중시한 쇼핑몰 개발과, 관리자가 쇼핑몰 사이트를 통합 관리할 수 있는 ERP 툴 개발을 결합한 프로젝트입니다.',
       link: '세 번째 프로젝트의 배포 링크',
-      github: '세 번째 프로젝트의 깃허브 링크'
+      github: '세 번째 프로젝트의 깃허브 링크',
+      review: '쇼핑몰 및 ERP 시스템 개발을 통해 실무적인 개발 경험을 쌓았습니다.',
+      technology: 'Node.js, MongoDB, React, Redux'
     }
   };
 
@@ -48,10 +53,10 @@ const ProjectDetails: React.FC<ProjectDetailsProps> = ({ project }) => {
           <p>{selectedProjectInfo.description}</p>
           <br />
           <h1>후기 및 소감</h1>
-          <p>후기입니다</p>
+          <p>{selectedProjectInfo.review}</p>
           <br />
-          <h1>STACK</h1>
-          <p>기술입니다</p>
+          <h1>기술 스택</h1>
+          <p>{selectedProjectInfo.technology}</p>
         </div>
       </div>
     </div>
