59a6689092fce7e0cff165ead3857ddc96ab3b07
bangseunghee <bangseunghee1108@gmail.com>
2024-03-18 16:23:28 +0900

    폰트추가

---
+++ server.js
@@ -0,0 +1,19 @@
+import express from "express";
+import next from "next";
+
+const dev = process.env.NODE_ENV !== "production";
+const app = next({ dev });
+const handle = app.getRequestHandler();
+
+app.prepare().then(() => {
+  const server = express();
+
+  server.get("*", (req, res) => {
+    return handle(req, res);
+  });
+
+  const PORT = process.env.PORT || 3000;
+  server.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
+});
