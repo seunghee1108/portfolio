46182156158d5d84a55a6c1e933482b395ff0371
bangseunghee <bangseunghee1108@gmail.com>
2024-03-26 13:05:40 +0900

    프로젝트 페이지 수정

--- app/project/page.tsx
+++ app/project/page.tsx
@@ -1,16 +1,10 @@
 /* eslint-disable @next/next/no-img-element */
-"use client";
-
+"use client"
 import { NextPage } from 'next';
-import React, { useRef } from "react";
+import React from "react";
 import styles from "@/app/styles/project.module.scss";
-import { Swiper, SwiperSlide } from "swiper/react";
-import SwiperCore, { Navigation, Scrollbar } from "swiper/core";
-import "swiper/css";
-import "swiper/css/navigation";
-import "swiper/css/pagination";
-import Image from "next/image";
 
+// 수정된 타입 정의
 interface ProjectPageProps {
   handleProjectClick: (project: string) => void;
 }
@@ -20,64 +14,54 @@ const ProjectPage: NextPage<ProjectPageProps> = ({ handleProjectClick }) => {
     handleProjectClick(project);
   };
 
-  const scrollToBox4 = () => {
-    const box4Element = document.getElementById("box4");
-    if (box4Element) {
-      box4Element.scrollIntoView({ behavior: "smooth" });
-    }
-  };
-
-  const projects = [
-    { name: "TOURMAPS", imageUrl: "/tourmapsmain.png", alt: "TOURMAPS", description: "Solo project" },
-    { name: "BOARD", imageUrl: "/boardmain.png", alt: "BOARD", description: "Solo project" },
-    { name: "ERP-PROJECT", imageUrl: "/erp.png", alt: "ERPPROJECT", description: "Team project" },
-    { name: "ERP-PROJECTgg", imageUrl: "/tourmapsmain.png", alt: "ERPPROJECT", description: "Team project" },
-    { name: "ERP-PROJECT", imageUrl: "/erp.png", alt: "ERPPROJECT", description: "Team project" },
-    { name: "ERP-PROJECTgg", imageUrl: "/tourmapsmain.png", alt: "ERPPROJECT", description: "Team project" }
-  ];
-
-  SwiperCore.use([Navigation, Scrollbar]);
-
   return (
     <div className={styles.project}>
-      <Swiper
-        className={styles.swiperslider}
-        spaceBetween={0} // 슬라이스 사이 간격
-        slidesPerView={3} // 보여질 슬라이스 수
-        navigation={true} // prev, next button
-        scrollbar={true}
-      >
-        {projects.map((project, index) => (
-          <SwiperSlide key={index}>
-            <div className={styles.imageContainer}>
-              <div className={styles.imageWrapper}>
-                <Image
-                  src={project.imageUrl}
-                  alt={project.alt}
-                  width={300}
-                  height={300}
-                  onClick={() => handleDetailClick(project.name)}
-                />
-              </div>
-              <div className={styles.caption}>
-                <p>0{index + 1}</p>
-                <h1>{project.name}</h1>
-                <p>{project.description}</p>
-                <a
-                  href="#box4"
-                  onClick={(e) => {
-                    e.preventDefault();
-                    handleDetailClick(project.name);
-                    scrollToBox4(); // 자세히 보기 클릭 시 box4로 스크롤
-                  }}
-                >
-                  자세히 보기 &gt;
-                </a>
-              </div>
-            </div>
-          </SwiperSlide>
-        ))}
-      </Swiper>
+           <div className={styles.imageContainer}>
+        <img
+          src="/tourmapsmain.png"
+          alt="TOURMAPS"
+          onClick={(e) => e.stopPropagation()}
+        />
+        <div className={styles.caption}>
+          <p>01</p>
+          <h1>TOURMAPS</h1>
+          <p>Solo project</p>
+          <a href="#box4" onClick={() => handleDetailClick("TOURMAPS")}>
+            자세히 보기 &gt;
+          </a>
+        </div>
+      </div>
+
+      <div className={styles.imageContainer}>
+        <img
+          src="/boardmain.png"
+          alt="BOARD"
+          onClick={(e) => e.stopPropagation()}
+        />
+        <div className={styles.caption}>
+          <p>02</p>
+          <h1>BOARD</h1>
+          <p>Solo project</p>
+          <a href="#box4" onClick={() => handleDetailClick("BOARD")}>
+            자세히 보기 &gt;
+          </a>
+        </div>
+      </div>
+      <div className={styles.imageContainer}>
+        <img
+          src="/erp.png"
+          alt="ERPPROJECT"
+          onClick={(e) => e.stopPropagation()}
+        />
+        <div className={styles.caption}>
+          <p>03</p>
+          <h1>ERP-PROJECT</h1>
+          <p>Team project</p>
+          <a href="#box4" onClick={() => handleDetailClick("ERP-PROJECT")}>
+            자세히 보기 &gt;
+          </a>
+        </div>
+      </div>
     </div>
   );
 };
